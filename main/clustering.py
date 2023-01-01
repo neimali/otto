fd8b04b783c46fac898bd1b2d5721d8656e6d35d
neimali <gaoyinghan1997@gmail.com>
2023-01-02 03:17:48 +0800

    optimize clustering

--- main/clustering.py
+++ main/clustering.py
@@ -14,17 +14,24 @@ class Clustering:
 
     def kmCluster(self, k):
         km = KMeans(n_clusters=k, random_state=24)
+        print('clustering on k=' + str(k) + ' is started')
         km.fit(self.vectors)
+        print('clustering finished')
         label = km.labels_
         res = pd.DataFrame({'aid': self.article, 'label': label})
         return res
 
     def articles_rank_by_label(self, data, cluster_res):
         res = {}
+        print('start calculating rank by label')
         article_rank = data.aid.value_counts()
-        for l in cluster_res['label'].unique():
-            articles = cluster_res.loc[cluster_res.label == l, 'aid'].unique()
-            res[l] = article_rank.loc[article_rank.index.isin(articles)].index.tolist()
+        article_rank = pd.DataFrame({'aid': article_rank.index, 'counts': article_rank.values})
+        article_rank = pd.merge(article_rank, cluster_res)
+        # for l in cluster_res['label'].unique():
+        #     articles = cluster_res.loc[cluster_res.label == l, 'aid'].unique()
+        #     res[l] = article_rank.loc[article_rank.index.isin(articles)].index.tolist()
+        for l in tqdm(article_rank.groupby('label')):
+            res[l[0]] = l[1].aid.tolist()
         return res
 
     def validation(self, test, test_res, clusters, rank):
@@ -38,13 +45,21 @@ class Clustering:
 
         pred = {}
         scores = []
-        clusters_df = pd.DataFrame(clusters).set_index('aid')
-        test_cluster = test.join(clusters_df, on='aid', how='left')
-        for s in tqdm(test_cluster.session.unique()):
-            session_label = test_cluster.loc[test_cluster.session == s, 'label'].value_counts().index[0]
+        # clusters_df = pd.DataFrame(clusters).set_index('aid')
+        # test_cluster = test.join(clusters_df, on='aid', how='left')
+        test_cluster = pd.merge(test, clusters, how='left', on='aid')
+        # for s in tqdm(test_cluster.session.unique()):
+        #     session_label = test_cluster.loc[test_cluster.session == s, 'label'].value_counts().index[0]
+        #     candidates = rank[session_label]
+        #     pred[s] = candidates[:20-len(test.loc[test.session == s])]
+        #     score = metric(pred[s], test_res.loc[test_res.session == 's', 'aid'].tolist())
+        #     scores.append(score)
+        print('validation started')
+        for s, res in tqdm(zip(test_cluster.groupby('session'), test_res.groupby('session'))):
+            session_label = s[1].label.mode().iloc[0]
             candidates = rank[session_label]
-            pred[s] = candidates[:20-len(test.loc[test.session == s])]
-            score = metric(pred[s], test_res.loc[test_res.session == 's', 'aid'].tolist())
+            pred[s[0]] = candidates[:20-len(s[1])]
+            score = metric(pred[s[0]], res[1].aid)
             scores.append(score)
 
         return sum(scores)/len(scores)
@@ -63,6 +78,20 @@ class Clustering:
         print('best k value is ' + str(best_k) + ' and the score is ' + str(best_score) + ' best_k has been return')
         return best_k
 
+if __name__ == '__main__':
+    dl = DataLoad('D:\OTTO\Data\\train.jsonl')
+    dl.sample_data(10)
+    df = dl.to_dataframe(dl.sample)
+    train, test, test_res = dl.data_split(df)
+    wv = WordToVec(df)
+    s = wv.time_session(2)
+    s = list(s.values())
+    vector = wv.train(s)
+    c = Clustering('km', vector)
+    cres = c.kmCluster(2)
+    rank = c.articles_rank_by_label(df, cres)
+    score = c.validation(test, test_res, cres, rank)
+    # k = c.fine_tune([2,3], df, test, test_res)
 
 
 
